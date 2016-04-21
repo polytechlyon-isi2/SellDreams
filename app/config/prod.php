37155f5e321e79eadfde119d16b1674ea79a4610
matthieuMoisson <matthieu.moisson38@gmail.com>
2016-04-21 17:24:43 +0200

    tentative pour meilleur visibilité

--- app/config/prod.php
+++ app/config/prod.php
@@ -15,6 +15,7 @@ $app['db.options'] = array(
 
 // MySQL config for OpenShift
 $app['db.options'] = array(
+    'charset'  => 'utf8',
     'host'     => getenv('OPENSHIFT_MYSQL_DB_HOST'),
     'dbname'   => getenv('OPENSHIFT_GEAR_NAME'),
     'user'     => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
